e9bef9cef4a4198afaa18f823c20638e31b9f5e1
nsantos06 <nicollasmk9@gmail.com>
2025-01-18 13:56:16 -0300

    Find product integration feito

--- src/usecase/product/find/find.product.integration.spec.ts
+++ src/usecase/product/find/find.product.integration.spec.ts
@@ -3,40 +3,51 @@ import ProductRepository from "../../../infrastructure/product/repository/sequel
 import Product from "../../../domain/product/entity/product";
 import FindProductUseCase from "./find.product.usecase";
 import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import {sequelize } from "../../../infrastructure/api/express";
 
-const product = new Product("123", "Bronzina", 15);
 
-const MockRepository = () => {
-  return {
-    find: jest.fn().mockReturnValue(Promise.resolve(product)),
-    findAll: jest.fn(),
-    create: jest.fn(),
-    update: jest.fn(),
-  }
-}
+
+
 
 
 describe("Test find product use case", () => {
+  let sequelize: Sequelize;
 
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+    });
+
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+  it("should find the product", async () => {
+    const productRepository = new ProductRepository();
+    const usecase = new FindProductUseCase(productRepository);
+    
+    const product = new Product("123", "P1", 10); 
+    await productRepository.create(product);
+  
+    const input = {
+      id: "123"
+    }
+  
+    const output = {
+      id: "123",
+      name: "P1",
+      price: 10,
+    }
+  
+    const result = await usecase.execute(input);
+    expect(result).toEqual(output);
+  
+  });
+  
 });  
 
-
-
-it("should find a product:", async() => {
-    const productRepository = MockRepository();
-    const usecase = new FindProductUseCase(productRepository)
-    
-    const input = {
-        id: "123",
-
-    }
-    const output = { 
-        id: "123",
-        name: "Bronzina",
-        price: 15,
-    }
-
-    const result = await usecase.execute(input); 
-
-    expect(result).toEqual(output);
-});
